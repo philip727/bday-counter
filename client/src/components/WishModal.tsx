ad5b682f4ee0b1c1dc087fdc6a71454730608ee4
philip727 <philster36@hotmail.co.uk>
2024-04-30 11:32:42 +0100

    profanity filter n stuff

--- client/src/components/WishModal.tsx
+++ client/src/components/WishModal.tsx
@@ -1,24 +1,45 @@
 import { Show, createSignal, type Component } from 'solid-js';
 import { Portal } from 'solid-js/web';
 import { modalEvents } from '../scripts/modal';
+import { Socket } from 'socket.io-client';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import { ClientProviderResponse, useClient } from './Client';
 
 const WishModal: Component = () => {
+    const [client] = useClient() as ClientProviderResponse;
     const [name, setName] = createSignal("");
+    const [show, setShow] = createSignal(false);
 
     modalEvents.on("open_name_modal", () => {
-        console.log("hay");
+        setShow(prev => !prev);
     })
 
-
     return (
-        <Show when={false}>
+        <Show when={show()}>
             <Portal>
                 <div class="absolute">
-                    <input id="name-input" value="" placeholder="Your name" />
+                    <input
+                        id="name-input"
+                        value=""
+                        placeholder="Your name"
+                        onkeyup={(e) => {
+                            setName(e.currentTarget.value);
+                        }}
+                    />
+                    <button
+                        onclick={() => {onWishClick(client(), name())}}
+                    >
+                        Send wishes
+                    </button>
                 </div>
             </Portal>
         </Show>
     )
+}
+
+const onWishClick = (client: Socket<DefaultEventsMap, DefaultEventsMap>, name: string) => {
+    console.log(`${name} wishes you a happy birthday`);
+    client.emit("wish", name);
 }
 
 export default WishModal;
--- server/main.js
+++ server/main.js
@@ -1,4 +1,4 @@
-import { createServer } from "http";
+import { createServer, request } from "http";
 import { Server } from "socket.io";
 
 const PORT = 4000;
@@ -10,10 +10,27 @@ const main = () => {
     ioServer.on("connection", (socket) => {
         console.log("Hay connection");
 
-        socket.on("wish", (name) => {
-            console.log(`${name} wishes you a happy birthday`);
+        socket.on("wish", async (name) => {
+            // Uses an api to make sure the name isn't profane
+            const response = await fetch(`https://www.purgomalum.com/service/containsprofanity?text=${name}`,
+                { method: "GET" }
+            );
 
+            if (!response.ok) {
+                const status = response.status;
+                console.warn(`Failed profanity filter request: ${status}`);
+                return;
+            }
 
+            // Response is plaintext, ion know why
+            let containsProfanity = await response.text();
+            if (containsProfanity == "true") {
+                console.log(`Failed to wish as name was deemed profane: ${name}`);
+                return;
+            }
+
+            // Sends event back to client so we can display the birthday wish yay
+            console.log(`${name} wished a happy birthday`);
             ioServer.emit("new_wish", name);
         });
 
