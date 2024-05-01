b630012c7771d4a3d2df22e5853e764f6cfb9452
philip727 <philster36@hotmail.co.uk>
2024-05-01 10:26:36 +0100

    modal styling done

--- client/src/components/WishModal.tsx
+++ client/src/components/WishModal.tsx
@@ -18,8 +18,11 @@ const WishModal: Component = () => {
         <Show when={show()}>
             <Portal>
                 <div class="absolute top-0 left-0 w-screen h-full flex justify-center items-center">
-                    <div class="h-20 w-32 border-2 border-black">
+                    <div class="h-48 w-72  border-[var(--darker-pastel-pink)] flex flex-col items-center justify-between py-6 
+                        bg-[var(--pastel-pink)] rounded-2xl shadow-one">
                         <input
+                            class="w-56 h-12 border-[var(--darker-pastel-pink)] 
+                                rounded-2xl shadow-two pl-2 tracking-widest font-semibold"
                             id="name-input"
                             value=""
                             placeholder="Your name"
@@ -28,9 +31,13 @@ const WishModal: Component = () => {
                             }}
                         />
                         <button
+                            class="w-56 h-16 bg-white rounded-2xl  
+                                border-[var(--darker-pastel-pink)] shadow-two"
                             onclick={() => { onWishClick(client(), name()) }}
                         >
-                            Send wishes
+                            <p class="font-semibold tracking-widest text-2xl text-[var(--pastel-pink)]">
+                                Send wishes 🎉
+                            </p>
                         </button>
                     </div>
                 </div>
@@ -40,7 +47,6 @@ const WishModal: Component = () => {
 }
 
 const onWishClick = (client: Socket<DefaultEventsMap, DefaultEventsMap>, name: string) => {
-    console.log(`${name} wishes you a happy birthday`);
     client.emit("wish", name);
 }
 
